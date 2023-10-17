62afb4694541fe6eef19f1e9b3723c9c71c3868f
Eric Glock <eric@glocks.net>
2023-10-16 21:57:11 -0400

    Integrate basic Home Assistant control
    1. Extend CONFIG with Home Assistant server details.
    2. Refactor get_api_request_via_completion to support Home Assistant API structure.
    3. Implement make_hass_request for direct interactions with Home Assistant.
    
    This commit allows the system to directly control Home Assistant via voice commands.

--- iris.py
+++ iris.py
@@ -4,6 +4,7 @@ import time
 import logging
 import traceback
 import json
+import requests
 from contextlib import contextmanager
 
 import openai
@@ -26,7 +27,9 @@ CONFIG = {
         'wake': '/System/Library/Sounds/Ping.aiff',
         'success': '/System/Library/Sounds/Glass.aiff',
         'fail': '/System/Library/Sounds/Basso.aiff'
-    }
+    },
+    'hass_address': '10.10.233.190',
+    'hass_port': '8123'
 }
 
 def init_picovoice_modules(access_key):
@@ -67,7 +70,7 @@ def save_frames_as_mp3(frames, file_path="output.mp3"):
     frames_bytes = b''.join(frames)
 
     # Calculate the number of bytes per frame: 2 bytes/sample * 1 channel
-    bytes_per_frame = 2 * 1  # Modify as needed based on your audio format
+    bytes_per_frame = 2 * 1  # Modify as needed based on audio format
 
     # Calculate if there are any leftover bytes and trim the frames_bytes if necessary
     remainder = len(frames_bytes) % bytes_per_frame
@@ -103,8 +106,79 @@ def transcribe_audio(file_path="output.mp3"):
         return None
 
 def get_api_request_via_completion(content):
-    completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": content}])
-    return completion.choices[0].message.content
+    home_assistant_function = {
+        "name": "make_hass_request",
+        "description": "Make a Home Assistant API request",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "method": {
+                    "type": "string",
+                    "enum": ["GET", "POST"]
+                },
+                "endpoint": {
+                    "type": "string"
+                },
+                "body": {
+                    "type": "string",
+                    "optional": True
+                }
+            },
+            "required": ["method", "endpoint"]
+        },
+        "response": {
+            "type": "object",
+            "properties": {
+                "message": {
+                    "type": "string"
+                }
+            }
+        }
+    }
+
+    completion = openai.ChatCompletion.create(
+        model="gpt-4",
+        messages=[
+            {
+                "role": "user",
+                "content": content
+            }
+        ],
+        temperature=0.1,
+        functions=[home_assistant_function]
+    )
+
+    call = json.loads(completion.choices[0].message.function_call.arguments)
+
+    return call['method'], call['endpoint'], call['body']
+
+def make_hass_request(method, endpoint='', body=None):
+    """Make a request to Home Assistant."""
+    try:
+        if method == "GET":
+            response = requests.get(
+                f"http://{CONFIG['hass_address']}:{CONFIG['hass_port']}/api/{endpoint}",
+                headers={
+                    'Authorization': f'Bearer {HASS_KEY}',
+                    'Content-Type': 'application/json'
+                },
+                data=body
+            )
+        elif method == "POST":
+            response = requests.post(
+                f"http://{CONFIG['hass_address']}:{CONFIG['hass_port']}/api/{endpoint}",
+                headers={
+                    'Authorization': f'Bearer {HASS_KEY}',
+                    'Content-Type': 'application/json'
+                },
+                data=body
+            )
+        return response.text
+    except Exception as e:
+        logger.error("An error occurred while making a request to Home Assistant: ")
+        logger.error(traceback.format_exc())
+        return None
+
 
 def process_audio_stream(porcupine, cobra, stream):
     """Main loop to process audio stream, detect wake word, and record command."""
@@ -138,9 +212,10 @@ def process_audio_stream(porcupine, cobra, stream):
                             logging.info('Silence detected, saving command to mp3 and resetting...')
                             if save_frames_as_mp3(frames):
                                 subprocess.Popen(["afplay", CONFIG['alert_sounds']['success']])
-                                return transcribe_audio()
+                                return True
                             else:
                                 subprocess.Popen(["afplay", CONFIG['alert_sounds']['fail']])
+                                return False
                             break
     except KeyboardInterrupt:
         logger.info("Stopping...")
@@ -152,9 +227,14 @@ def main():
         openai.api_key = OPENAI_KEY
         porcupine, cobra = init_picovoice_modules(PV_KEY)
         with audio_stream(p) as stream:
-            transcription = process_audio_stream(porcupine, cobra, stream)
-            api_request = get_api_request_via_completion(transcription)
-            print(api_request)
+            processed = process_audio_stream(porcupine, cobra, stream)
+        if processed:
+            transcription = transcribe_audio()
+            print(f"User: {transcription}")
+            method, endpoint, body = get_api_request_via_completion(transcription)
+            logging.info(f"Method: {method}, Endpoint: {endpoint}, Body: {body}")
+            response = make_hass_request(method, endpoint, body)
+            logging.info(response)
     except Exception as e:
         logger.error("An error occurred: ")
         logger.error(traceback.format_exc())
